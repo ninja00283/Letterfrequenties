18ac3fc961efcf9e83be5a9c3902794fbd8e0e8f
jan bos <max.bos217@gmail.com>
2019-01-06 16:58:42 +0100

    done

--- Letter/Form1.Designer.cs
+++ Letter/Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.CharacterCountTextFlowBox = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "choose file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ChooseFileClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Location = new System.Drawing.Point(93, 12);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.Size = new System.Drawing.Size(110, 20);
+            this.PathTextBox.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(209, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Count";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.CountCharacters);
+            // 
+            // CharacterCountTextFlowBox
+            // 
+            this.CharacterCountTextFlowBox.AutoScroll = true;
+            this.CharacterCountTextFlowBox.Location = new System.Drawing.Point(12, 41);
+            this.CharacterCountTextFlowBox.Name = "CharacterCountTextFlowBox";
+            this.CharacterCountTextFlowBox.Size = new System.Drawing.Size(272, 261);
+            this.CharacterCountTextFlowBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(296, 314);
+            this.Controls.Add(this.CharacterCountTextFlowBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.PathTextBox);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(312, 353);
+            this.MinimumSize = new System.Drawing.Size(312, 353);
+            this.Name = "Form1";
+            this.Text = "Letterfrequenties";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel CharacterCountTextFlowBox;
     }
 }
 
--- Letter/Form1.cs
+++ Letter/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,127 @@ namespace Letter
 {
     public partial class Form1 : Form
     {
+
+        string FilePath = "";
+        bool HasFile = false;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
+
+        private void ChooseFileClick(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                if (Path.GetExtension(file) == ".txt")
+                {
+                    try
+                    {
+                        string text = File.ReadAllText(file);
+                        size = text.Length;
+                        FilePath = file;
+                    }
+                    catch (IOException)
+                    {
+                    }
+                    PathTextBox.Text = FilePath;
+                    HasFile = true;
+
+                }
+                else
+                {
+                    //error not text
+                }
+
+            }
+
+        }
+
+        private void CountCharacters(object sender, EventArgs e)
+        {
+            if (HasFile) {
+                Debug.Print("Counting");
+                string TextToProcess = File.ReadAllText(FilePath);
+                char[] TextToProcessArray = TextToProcess.ToArray();
+
+                List<LetterCountClass> LetterCountList = new List<LetterCountClass>();
+
+                Debug.Print("To Count" + TextToProcessArray.Count().ToString());
+
+                
+
+                for (int CurrentLetter = 0; CurrentLetter < TextToProcessArray.Count(); CurrentLetter++)
+                {
+                    
+                    char CurrentCharacterToCount = TextToProcessArray[CurrentLetter].ToString().ToLower().ToCharArray()[0];
+                    Debug.Print(CurrentLetter.ToString() + "-"+ CurrentCharacterToCount);
+
+                    bool LetterInList = false;
+
+                    for (int ListPosition = 0; ListPosition < LetterCountList.Count(); ListPosition++)
+                    {
+                        if (LetterCountList[ListPosition].Letter == CurrentCharacterToCount) {
+                            LetterCountList[ListPosition].AddCount();
+                            LetterInList = true;
+                        }
+                    }
+
+                    if (!LetterInList) {
+                        LetterCountList.Add(new LetterCountClass(CurrentCharacterToCount,1));
+                    }
+
+                }
+
+                for (int ListPosition = 0; ListPosition < LetterCountList.Count(); ListPosition++)
+                {
+                    Debug.Print(LetterCountList[ListPosition].Letter.ToString() + "-" + LetterCountList[ListPosition].TimesSeen.ToString() + "\n" );
+                }
+
+                DisplayCharacters(LetterCountList);
+
+            }
+        }
+
+        private void DisplayCharacters(List<LetterCountClass> LetterList) {
+
+            while (CharacterCountTextFlowBox.Controls.Count > 0)
+            {
+                CharacterCountTextFlowBox.Controls[0].Dispose();
+            }
+
+            for (int CurrentListPos = 0; CurrentListPos < LetterList.Count(); CurrentListPos++)
+            {
+                Label CharacterCountText = new Label();
+                CharacterCountText.Text = LetterList[CurrentListPos].Letter.ToString() + "-" + LetterList[CurrentListPos].TimesSeen.ToString();
+                CharacterCountText.Parent = CharacterCountTextFlowBox;
+                CharacterCountTextFlowBox.Controls.Add(CharacterCountText);
+            }
+
+        }
+
+
+        public class LetterCountClass {
+            public char Letter { get; set; }
+            public int TimesSeen { get; set; }
+
+            public LetterCountClass(char letter, int timesSeen)
+            {
+                Letter = letter;
+                TimesSeen = timesSeen;
+            }
+
+            public int AddCount()
+            {
+                TimesSeen++;
+                return TimesSeen;
+            }
+        }
+
     }
 }
